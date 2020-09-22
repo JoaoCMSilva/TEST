b8ccf86b66d93d9e1de08e7f51dc083b85476c5b
João Carlos Madeira Silva <joao-carlos-silva@telecom.pt>
2020-09-22 11:10:42 +0100

    optimizado a class imóvel

--- Imóvel.js
+++ Imóvel.js
@@ -1,10 +1,12 @@
-function Imóvel(rua, número, cidade, códigoPostalPrincipal, códigoPostalsecundário, valor) {
+function Imóvel(rua, número, cidade, códigoPostalPrincipal, códigoPostalsecundário, valor, cor) {
 
 	this.rua = rua;
 	this.número = número;
 	this.cidade = cidade;
-	this.códigoPostalPrincipal = códigoPostalPrincipal;
+	this.códigoPostalPrincipal = códigoPostalPrincipal; //as 2 partes do CP
 	this.códigoPostalsecundário = códigoPostalsecundário;
 	this.valor = valor;
+
+	this.cor = cor;
 
 }
